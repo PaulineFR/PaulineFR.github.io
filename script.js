a772fa6f04bfcb26c073825260e5bbda7bb0a8c1
PaulineFR <pauline.franquelin@gmail.com>
2024-11-18 19:51:04 +0100

    numéro de la semaine affiché + toggle

--- script.js
+++ script.js
@@ -34,6 +34,10 @@ function isMonday(day) {
    return (day  === 1);
 }
 
+function isWednersday(day) {
+   return (day  === 3);
+}
+
 function getDayLetter(day) {
    switch (day){
     case 1:
@@ -51,6 +55,39 @@ function getDayLetter(day) {
     case 0:
       return "D";
   }
+}
+
+/* For a given date, get the ISO week number
+ *
+ * Based on information at:
+ *
+ *    THIS PAGE (DOMAIN EVEN) DOESN'T EXIST ANYMORE UNFORTUNATELY
+ *    http://www.merlyn.demon.co.uk/weekcalc.htm#WNR
+ *
+ * Algorithm is to find nearest thursday, it's year
+ * is the year of the week number. Then get weeks
+ * between that date and the first day of that year.
+ *
+ * Note that dates in one year can be weeks of previous
+ * or next year, overlap is up to 3 days.
+ *
+ * e.g. 2014/12/29 is Monday in week  1 of 2015
+ *      2012/1/1   is Sunday in week 52 of 2011
+ * 
+ * source :  https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php
+ */
+function getWeekNumber(d) {
+  // Copy date so don't modify original
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  // Set to nearest Thursday: current date + 4 - current day number
+  // Make Sunday's day number 7
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+  // Get first day of year
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+  // Calculate full weeks to nearest Thursday
+  var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+  // Return array of year and week number
+  return [d.getUTCFullYear(), weekNo];
 }
 
 
@@ -71,7 +108,7 @@ function generate(){
   const TEMPLATE   = year == 0;
   const U_WEEKENDS = !TEMPLATE && document.getElementById("weekend").checked; // weekend affichés
   const U_WEEKS    = !TEMPLATE && document.getElementById("week").checked; // semaines affcihés
-  const U_NOWEEEK  = !TEMPLATE; // no semaine affichés
+  const U_NOWEEEK  = !TEMPLATE && document.getElementById("weekno").checked; // no semaine affichés
   const U_WEEKDAY  = !TEMPLATE && document.getElementById("weekday").checked; // jour semaine affiché
   const U_PAST     = !TEMPLATE && document.getElementById("past").checked; // past blacked out
   const U_VERTICAL = document.getElementById("vertical").checked; // vertical
@@ -249,6 +286,23 @@ function generate(){
         text.textContent = getDayLetter(circle_date.getDay());
         svg.appendChild(text);
       }
+
+      if(U_NOWEEEK && isWednersday(circle_date.getDay())){
+        // Ajouter le label du jour
+        const text = document.createElementNS(svgNS, "text");
+        const weekno = Number(getWeekNumber(circle_date)[1]);
+        
+        const days_axis = header + margin + day * (cellSize + spacing) - cellSize/4 ;
+        const month_axis = header + margin + (month + 1) * (cellSize + spacing) - cellSize/4;
+        text.setAttribute("x", (U_VERTICAL ? month_axis : days_axis) + (weekno < 10 ? 1 : -1) * 0.8);
+        text.setAttribute("y", (!TEMPLATE ? title : 0) + (U_VERTICAL ? days_axis : month_axis));
+        text.setAttribute("font-size", "6");
+        text.setAttribute("font-family", "Arial");
+        text.setAttribute("dominant-baseline", "hanging");
+        text.setAttribute("fill", "black");
+        text.textContent = weekno;
+        svg.appendChild(text);
+      }
     }
   }
 
@@ -256,3 +310,18 @@ function generate(){
   document.getElementById("svg-container").innerHTML = "";
   document.getElementById("svg-container").appendChild(svg);
 }
+
+
+function toggleCircleTextNo(){
+  document.getElementById("weekno").checked = false; // no semaine non affichés
+  generate();
+  
+}
+
+function toggleCircleTextLetter(){
+  document.getElementById("weekday").checked = false; // jour semaine non affiché
+  generate();
+  
+}
+
+
